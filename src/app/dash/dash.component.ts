4e231e63388262d9de9bf5884d14499f4b57316f
Scott Townson <stownson@dashcomputer.co.uk>
2020-10-23 15:28:44 +0100

    fixed table overflow on mobile

--- src/app/dash/dash.component.ts
+++ src/app/dash/dash.component.ts
@@ -16,7 +16,7 @@ export class DashComponent {
           columns: 1,
           //miniCard: { cols: 1, rows: 1 },
           chart: { cols: 1, rows: 2 },
-          table: { cols: 1, rows: 4 }
+          table: { cols: 1, rows: 3 }
         };
       }
 
